d62c3cc94c5ac91212cc48b733e5cc09967f95dd
Jay Koutavas <jay@heynow.com>
2017-07-28 20:38:52 -0400

    Updated map data

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -25,7 +25,7 @@ export class MapPage {
 
         let map = new google.maps.Map(mapEle, {
           center: mapData.find((d: any) => d.center),
-          zoom: 16
+          zoom: 5
         });
 
         mapData.forEach((markerData: any) => {

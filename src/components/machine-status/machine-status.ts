c12d2f332773d3071c5b4f60074115717a7cc0e0
Jay Koutavas <jay@heynow.com>
2017-08-22 17:43:12 -0400

    machine-state component is now current time sensitive (and dynamic)

--- src/components/machine-status/machine-status.ts
+++ src/components/machine-status/machine-status.ts
@@ -14,38 +14,37 @@ import { DowntimeData } from '../../providers/downtime-data';
 })
 export class MachineStatusComponent {
 
-  @Input()
-  set machine(m:any) {
-    
-    // compute maxiumum expected uptime (in seconds)
-    let uptime : number = 0;
-
-    let downtime : number = 0;
-    const events = this.downtimeData.getDowntimeEvents().filter(function(event:any){
-      return event.machineId == m.id;
-    });
-    for( const event of events ) {
-      if( event ) {
-        if( event.id && event.codeId != 15864 /* scheduled downtime*/ && event.codeId != 16024 /* end of shift */ ) {
-          downtime += event.endTime - event.startTime;
-        } else {
-          if( uptime + (event.endTime - event.startTime) < 0 ) {
-            console.log("foo");
-          }
-
-          uptime += event.endTime - event.startTime;
-
-        }
-      }
-    }
-
-    const percent = (uptime / (downtime + uptime)) * 100;
-    this.text = "Availability: " + percent.toFixed(2) + "%";
-  }
-
+  @Input() machine:any;
+  
   text: string;
 
   constructor(private downtimeData: DowntimeData) {
   }
 
+  ngOnInit() {
+    let me = this;
+    this.downtimeData.getClock().subscribe(time => {
+
+      // compute maxiumum expected uptime (in seconds)
+      let uptime : number = 0;
+      let downtime : number = 0;
+      const events = this.downtimeData.getDowntimeEvents().filter(function(event:any){
+        return event.machineId == me.machine.id;
+      });
+      for( const event of events ) {
+        if( event ) {
+          if( event.startTime < time &&
+              event.id && 
+              event.codeId != 15864 /* scheduled downtime*/ && 
+              event.codeId != 16024 /* end of shift */ ) {
+            downtime += event.endTime - event.startTime;
+          }
+          uptime += event.endTime - event.startTime;
+        }
+      }
+  
+      const percent = (uptime / (downtime + uptime)) * 100;
+      me.text = "Availability: " + percent.toFixed(2) + "%";    
+    });
+  }
 }
